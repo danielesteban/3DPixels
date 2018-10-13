6cf63c46976779cb02b5af581806804cdd567a7e
Daniel Esteban Nombela <dani@gatunes.com>
2018-10-13 11:56:06 +0200

    Ground grid shader +tweak pointer animations

--- client/components/meshes/ground.js
+++ client/components/meshes/ground.js
@@ -1,7 +1,9 @@
 import {
   Mesh,
-  MeshBasicMaterial,
   PlaneBufferGeometry,
+  ShaderLib,
+  ShaderMaterial,
+  UniformsUtils,
 } from 'three';
 
 class Ground extends Mesh {
@@ -9,10 +11,46 @@ class Ground extends Mesh {
     const geometry = new PlaneBufferGeometry(1, 1);
     geometry.rotateX(Math.PI * -0.5);
     geometry.translate(0, -0.001, 0);
+    const vertexShader = ShaderLib.basic.vertexShader.replace(
+      '#include <clipping_planes_pars_vertex>',
+      [
+        '#include <clipping_planes_pars_vertex>',
+        'varying vec3 vWorldPosition;',
+      ].join('\n')
+    ).replace(
+      '#include <fog_vertex>',
+      [
+        '#include <fog_vertex>',
+        'vWorldPosition = (modelMatrix * vec4( transformed, 1.0 )).xyz;',
+      ].join('\n')
+    );
+    const fragmentShader = ShaderLib.basic.fragmentShader.replace(
+      '#include <clipping_planes_pars_fragment>',
+      [
+        '#include <clipping_planes_pars_fragment>',
+        'varying vec3 vWorldPosition;',
+      ].join('\n')
+    ).replace(
+      'vec4 diffuseColor = vec4( diffuse, opacity );',
+      [
+        'vec2 coord = vWorldPosition.xz * 0.125;',
+        'vec2 grid = abs(fract(coord - 0.5) - 0.5) / fwidth(coord);',
+        'float line = min(grid.x, grid.y);',
+        'vec4 diffuseColor = vec4(mix(diffuse, diffuse * 1.25, 1.0 - min(line, 1.0)), opacity);',
+      ].join('\n')
+    );
     super(
       geometry,
-      new MeshBasicMaterial()
+      new ShaderMaterial({
+        name: 'ground-material',
+        uniforms: UniformsUtils.clone(ShaderLib.basic.uniforms),
+        fragmentShader,
+        vertexShader,
+        extensions: { derivatives: true },
+        fog: true,
+      })
     );
+    this.material.color = this.material.uniforms.diffuse.value;
     this.scale.set(512, 1, 512);
   }
 }
--- client/components/renderer.vue
+++ client/components/renderer.vue
@@ -162,17 +162,17 @@ export default {
       } = this.state;
       renderer.animationDelta = clock.getDelta();
       renderer.animationTime = clock.oldTime / 1000;
-      const maxAngle = Math.PI * 0.45;
+      const maxAngle = Math.PI * 0.3;
       if (pointer.active) {
         const step = renderer.animationDelta * 0.2;
         pan.rotation.y -= pointer.movement.x * step;
         pan.rotation.y = Math.min(Math.max(pan.rotation.y, -maxAngle), maxAngle);
         tilt.rotation.x -= pointer.movement.y * step * 0.5;
-        tilt.rotation.x = Math.min(Math.max(tilt.rotation.x, -maxAngle), maxAngle);
+        tilt.rotation.x = Math.min(Math.max(tilt.rotation.x, -maxAngle), maxAngle * 0.5);
         pointer.movement.x = 0;
         pointer.movement.y = 0;
       } else if (pointer.animation && fullscreen.target() !== mount) {
-        const step = renderer.animationDelta;
+        const step = renderer.animationDelta * 0.5;
         const smooth = delta => (
           Math.min(Math.max(delta, -step), step)
         );
@@ -181,7 +181,7 @@ export default {
             (pointer.normalized.x * maxAngle) - pan.rotation.y
           );
           tilt.rotation.x += smooth(
-            (pointer.normalized.y * maxAngle) - tilt.rotation.x
+            (pointer.normalized.y * maxAngle * 0.5) - tilt.rotation.x
           );
         } else {
           pan.rotation.y += smooth(-pan.rotation.y);

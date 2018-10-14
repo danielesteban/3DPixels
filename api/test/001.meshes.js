c729d1101c0399af29ce37f63a7f964d43e75252
Daniel Esteban Nombela <dani@gatunes.com>
2018-10-14 04:52:33 +0200

    Test mesh updates

--- api/test/001.meshes.js
+++ api/test/001.meshes.js
@@ -7,12 +7,17 @@ const testMesh = {
   bg: 0xFFFFFF,
   fps: 2,
   title: 'test',
+  titleUpdate: 'testUpdate',
   texture: Buffer.from(
     'iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAAhElEQVR4Xu3VAREAMAwCseLfdIV85qAcGbv4W/z+E4AGxBNAIF4AnyACCMQTQCBeACuAAALxBBCIF8AKIIBAPAEE4gWwAgggEE8AgXgBrAACCMQTQCBeACuAAALxBBCIF8AKIIBAPAEE4gWwAgggEE8AgXgBrAACCMQTQCBeACuAQJ3AA2jYAEGs/2CBAAAAAElFTkSuQmCC',
     'base64'
   ),
   pixel: Buffer.from(
     'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAC0lEQVQYV2NgAAIAAAUAAarVyFEAAAAASUVORK5CYII',
+    'base64'
+  ),
+  update: Buffer.from(
+    'iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAAlElEQVR4Xu3WwQGAIAADsbL/0PhRl7i4AbWhnLvds51Fv+zBv/8tgGjz/2NrgAbEE0AgXoDuA8g74E3AHeAOiCeAQLwAZhABBOIJIBAvgBVAAIF4AgjEC2AFEEAgngAC8QJYAQQQiCeAQLwAVgABBOIJIBAvgBVAAIF4AgjEC2AFEEAgngAC8QJYAQQQiCeAQLwAewDrUAJB6KE28QAAAABJRU5ErkJggg',
     'base64'
   ),
 };
@@ -79,6 +84,86 @@ describe('Create a mesh', () => {
       .expect(200)
       .then(({ body: texture }) => {
         assert(texture.equals(testMesh.texture));
+      })
+  ));
+});
+
+describe('Update a mesh', () => {
+  it('PUT /meshes/:id without a token should return a 401', () => (
+    request(api)
+      .put(`/meshes/${api.get('testMeshID')}`)
+      .expect(401)
+  ));
+  it('PUT /meshes/:id without params should return a 422', () => (
+    request(api)
+      .put(`/meshes/${api.get('testMeshID')}`)
+      .set('Authorization', `Bearer ${api.get('testUserToken')}`)
+      .expect(422)
+  ));
+  it('PUT /meshes/:id with a bad texture should return a 422', () => (
+    request(api)
+      .put(`/meshes/${api.get('testMeshID')}`)
+      .set('Authorization', `Bearer ${api.get('testUserToken')}`)
+      .field('bg', testMesh.bg)
+      .field('fps', testMesh.fps)
+      .field('title', testMesh.titleUpdate)
+      .attach('texture', Buffer.from([0, 0, 0]), 'texture.png')
+      .expect(422)
+  ));
+  it('PUT /meshes/:id with an incorrect texture size should return a 422', () => (
+    request(api)
+      .put(`/meshes/${api.get('testMeshID')}`)
+      .set('Authorization', `Bearer ${api.get('testUserToken')}`)
+      .field('bg', testMesh.bg)
+      .field('fps', testMesh.fps)
+      .field('title', testMesh.titleUpdate)
+      .attach('texture', testMesh.pixel, 'texture.png')
+      .expect(422)
+  ));
+  it('GET /meshes/:id with a bad id should return a 422', () => (
+    request(api)
+      .put('/meshes/badid')
+      .set('Authorization', `Bearer ${api.get('testUserToken')}`)
+      .field('bg', testMesh.bg)
+      .field('fps', testMesh.fps)
+      .field('title', testMesh.titleUpdate)
+      .attach('texture', testMesh.update, 'texture.png')
+      .expect(422)
+  ));
+  it('GET /meshes/:id with an unknown id should return a 404', () => (
+    request(api)
+      .put('/meshes/000000000000000000000000')
+      .set('Authorization', `Bearer ${api.get('testUserToken')}`)
+      .field('bg', testMesh.bg)
+      .field('fps', testMesh.fps)
+      .field('title', testMesh.titleUpdate)
+      .attach('texture', testMesh.update, 'texture.png')
+      .expect(404)
+  ));
+  it('PUT /meshes/:id should return a 200', () => (
+    request(api)
+      .put(`/meshes/${api.get('testMeshID')}`)
+      .set('Authorization', `Bearer ${api.get('testUserToken')}`)
+      .field('bg', testMesh.bg)
+      .field('fps', testMesh.fps)
+      .field('title', testMesh.titleUpdate)
+      .attach('texture', testMesh.update, 'texture.png')
+      .expect(200)
+  ));
+  it('GET /meshes/:id should return the updated mesh meta', () => (
+    request(api)
+      .get(`/meshes/${api.get('testMeshID')}`)
+      .expect(200)
+      .then(({ body: { title } }) => {
+        assert(title === testMesh.titleUpdate);
+      })
+  ));
+  it('GET /meshes/:id/texture should return the updated mesh texture', () => (
+    request(api)
+      .get(`/meshes/${api.get('testMeshID')}/texture`)
+      .expect(200)
+      .then(({ body: texture }) => {
+        assert(texture.equals(testMesh.update));
       })
   ));
 });
